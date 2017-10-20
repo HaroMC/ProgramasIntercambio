38c3d2fad6dd86513b3f97652386b6641e48dbdd
HaroMC <>
2017-10-20 15:34:58 -0300

    Agregados comentarios sobre el funcionamiento de métodos de PersonaDAO. Trabajando en terminar los métodos restantes de la clase.

--- ProgramasIntercambio/src/java/org/cem/dao/PersonaDAO.java
+++ ProgramasIntercambio/src/java/org/cem/dao/PersonaDAO.java
@@ -1,6 +1,7 @@
 package org.cem.dao;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import org.cem.connection.HibernateUtil;
@@ -9,14 +10,37 @@ import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+/**
+ * Clase pública que se encarga de manipular los objetos tipo
+ * <code>Persona</code> dentro de la base de datos.
+ * 
+ * @author HaroMC
+ * @version 1.1
+ */
 public class PersonaDAO implements ICrud {
     
     private final Session session;
     
+    /**
+     * Constructor vacío que inicializa la conexión al momento de instanciar
+     * el DAO.
+     */
     public PersonaDAO() {
         this.session = HibernateUtil.getSessionFactory().openSession();
     }
     
+    /**
+     * Este método de encarga de agregar un objeto, recibido por parámetro,
+     * a la base de datos. Al implementar Hibernate, este método no requiere
+     * ejecutar ninguna sentencia HQL, sino que, sólamente, se vale del método
+     * <code>.save(Object)</code> para realizar la inserción.
+     * 
+     * @param objParam Objeto que recibe el método para ser agregado a la
+     * base de datos. Este objeto puede ser de cualquier tipo de las clases
+     * definidas por Hibernate.
+     * @return <code>true</code> si el objeto fue insertado correctamente o
+     * <code>false</code> en el caso de que no se pueda agregar.
+     */
     @Override
     public boolean agregar(Object objParam) {
         boolean resultado = false;
@@ -35,17 +59,32 @@ public class PersonaDAO implements ICrud {
         }
         return resultado;
     }
-
+    
+    /**
+     * 
+     * @param objParam
+     * @param id
+     * @return 
+     */
     @Override
     public boolean modificar(Object objParam, int id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Método aún no implementado.");
     }
 
+    /**
+     * 
+     * @param id
+     * @return 
+     */
     @Override
     public boolean eliminar(int id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Método aún no implementado.");
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
     public List<Object> obtenerListado() {
         List<Object> personas = null;
@@ -54,7 +93,7 @@ public class PersonaDAO implements ICrud {
             personas = (List<Object>) hql.list();
         }
         catch (HibernateException hex) {
-            System.out.println("Error: " + hex);
+            throw new HibernateException("Error: ", hex);
         }
         finally {
             session.close();
@@ -62,16 +101,33 @@ public class PersonaDAO implements ICrud {
         return personas;
     }
 
+    /**
+     * 
+     * @param id
+     * @return 
+     */
     @Override
     public Object buscarPorID(int id) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public BigDecimal buscarUltimoID() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Método aún no implementado.");
     }
     
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public BigDecimal buscarUltimoID() {
+        throw new UnsupportedOperationException("Método aún no implementado.");
+    }
+    
+    /**
+     * Método que transforma el listado de objetos tipo <code>Object</code>
+     * que genera el método <code>obtenerListado</code> a una lista de objetos
+     * de tipo <code>Persona</code>.
+     * 
+     * @param listado <code>List</code> de tipo <code>Object</code>.
+     * @return <code>List</code> de tipo <code>Persona</code>.
+     */
     public List<Persona> convertirListado(List<Object> listado) {
         List<Persona> personas = new ArrayList<>();
         for (Object object : listado) {
