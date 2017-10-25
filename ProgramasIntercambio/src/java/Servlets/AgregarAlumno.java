aa3f46bd120bc799705d276fb2097c34d58ae7ff
David <David@DiskTape>
2017-10-25 20:46:26 -0300

    ediciones servlets

--- ProgramasIntercambio/src/java/Servlets/AgregarAlumno.java
+++ ProgramasIntercambio/src/java/Servlets/AgregarAlumno.java
@@ -7,11 +7,13 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.cem.entities.Alumno;
 
 /**
  *
@@ -19,60 +21,25 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "AgregarAlumno", urlPatterns = {"/AgregarAlumno"})
 public class AgregarAlumno extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AgregarAlumno</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AgregarAlumno at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        Alumno a = new Alumno();
+        //AQUI VA PERSONA
+        a.setRutPersona(Integer.parseInt(request.getParameter("rutPersona")));
+        //Matricula parseada
+        a.setNumeroMatricula(BigDecimal.valueOf(Double.parseDouble(request.getParameter("numeroMatricula"))));
+        //AQUI VA EL PARSEO DE LA FECHA MATRICULA
+        
+        
+        
+        
+        //-------DAO
+        
+        //1.- DAO ALUMNO
+        //INSERTAR ALUMNO a 
+        
     }
 
     /**
