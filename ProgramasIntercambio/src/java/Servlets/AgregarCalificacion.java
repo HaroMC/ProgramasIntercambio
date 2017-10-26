55c919876d393ef3653a292a4b5bc34d418d0cbc
David <David@DiskTape>
2017-10-25 21:13:20 -0300

    ed

--- ProgramasIntercambio/src/java/Servlets/AgregarCalificacion.java
+++ ProgramasIntercambio/src/java/Servlets/AgregarCalificacion.java
@@ -7,11 +7,14 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.cem.entities.Alumno;
+import org.cem.entities.Calificacion;
 
 /**
  *
@@ -20,69 +23,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "AgregarCalificacion", urlPatterns = {"/AgregarCalificacion"})
 public class AgregarCalificacion extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AgregarCalificacion</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AgregarCalificacion at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        Calificacion c = new Calificacion();
+        c.setCodigo(BigDecimal.valueOf(Double.parseDouble(request.getParameter("codigoCalificación"))));
+        //Aqui va el alumno (rut)
+        Alumno a = new Alumno();
+        a.setRutPersona(Integer.parseInt(request.getParameter("rutAlumno")));
+        //DAO ALUMNO .buscarAlumno(a)
+        if (true) {
+        //SI EXISTE
+            c.setAlumno(a);
+            //AQUI VA FECHA PARSEADA
+            c.setNota(BigDecimal.valueOf(Double.parseDouble(request.getParameter("notaCalificacion"))));
+            
+        }else{//SINO MENSAJE DE ALUMNO INEXISTENTE
+        
+        }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
